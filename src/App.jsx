0505841319a4ea219baa9b2db4aa7bbb7540d8b9
caio300 <caioqs.info@gmail.com>
2022-07-29 14:07:49 -0300

    Finalizado css componente Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header';
 import { Search } from './components/Search';
 import { InsistentTasks } from './components/InsistentTasks';
+import { Tasks } from './components/Tasks';
 
 import style from './App.module.css';
 import '../global.css';
@@ -18,7 +19,13 @@ function App() {
           <p className={style.createdTasks}>Tarefaz criadas <span>0</span></p>
           <p className={style.doneTasks}>Concluídas <span>0</span></p>
         </div>
-        <InsistentTasks />
+        {/* <InsistentTasks /> */}
+        <Tasks id={1}/>
+        <Tasks id={2}/>
+        <Tasks id={3}/>
+        <Tasks id={4}/>
+        <Tasks id={5}/>
+        <Tasks id={6}/>
       </div>
     </div>
   )
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,27 @@
+import { Trash, Check } from 'phosphor-react';
+import style from './Tasks.module.css';
+
+
+export function Tasks({ id }) {
+  return (
+    <div className={style.tasks}>
+      <input className={style.inputRadio} type="radio" id={id} name={id}/>
+      <label htmlFor={id}>
+        <div className={style.btnRadio}></div>
+        <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
+      </label>
+      <div className={style.trash}>
+        <Trash size={16}/>
+      </div>
+    </div>
+  )
+}
+
+
+{/* <select className={style.tasks}>
+      <input type="radio" name='1'/>
+      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      <div>
+        <Trash size={16}/>
+      </div>
+    </select> */}
